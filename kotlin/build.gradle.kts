69449c9d396bff3015021cbe223489820bfd1176
David Boney <dboney@openteams.com>
2022-06-02 10:01:58 -0500

    updated build instructions, updated to jvm 11, updated libtorch

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -26,12 +26,12 @@ allprojects {
     apply(plugin = "java")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     tasks.withType<KotlinCompile>() {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
         kotlinOptions.freeCompilerArgs += "-XXLanguage:+ProperCheckAnnotationsTargetInTypeUsePositions"
     }
 
