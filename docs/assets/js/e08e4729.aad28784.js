69432c57ba30642baba372685a2843450d523496
David Boney <95656431+dboney-openteams@users.noreply.github.com>
2022-06-02 12:12:58 -0500

    Clean up gh-pages (#76)
    * cleaned out docs
    
    * new docs
    
    * cleaned docs
    
    * cleaned out docs
    
    * new docs
    
    * cleaned out docs
    
    * new docs
    
    * cleaned out docs
    
    * new docs

--- docs/assets/js/e08e4729.aad28784.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[6749],{8651:function(e){e.exports=JSON.parse('{"permalink":"/diffkt/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
