44d8d0c25eabf68a90682c8d9b15eca340a1f2d6
David Boney <95656431+dboney-openteams@users.noreply.github.com>
2022-07-07 23:22:49 -0400

    Update to gh-pages with latest website. (#102)
    * cleaned out docs
    
    * new docs

--- docs/assets/js/2bd938c2.c582c5b2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[429],{3769:function(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
