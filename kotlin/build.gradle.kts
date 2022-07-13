2d523b50ec212b509b2718a5555ecec3e90dae8d
Steffi Stumpos <stumpos@fb.com>
2022-07-13 17:45:20 -0600

    revert to 8 in order to support adoptimize

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -26,12 +26,12 @@ allprojects {
     apply(plugin = "java")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     tasks.withType<KotlinCompile>() {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "1.8"
         kotlinOptions.freeCompilerArgs += "-XXLanguage:+ProperCheckAnnotationsTargetInTypeUsePositions"
     }
 
